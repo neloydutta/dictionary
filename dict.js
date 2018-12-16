958550e0856c33881efbdf898941a125f9916aef
Neloy Dutta <neloy.dutta@live.com>
2018-12-16 15:57:38 +0530

    updated, word array

--- dict.js
+++ dict.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 var readline = require('readline-sync');
 var app_id = "";
 var app_key = "";
-var usage = "Usage:\n./dict <def|syn|ant> <word>";
+var usage = "Usage:\nnode dict.js <def|syn|ant> <word>\nor\nnode dict.js play\nor\nnode dict.js";
 var word = "";
-var word_arr = ['good','bad','simple','extraordinary']
+var word_arr = ['discriminate', 'superb', 'compunction', 'prevail', 'ellipsis', 'cohere', 'opulence', 'conserve', 'quadrennium', 'privy', 'conifer', 'specify', 'icon', 'forecast', 'intervene', 'import', 'reduce', 'taint', 'inventory', 'contrived', 'laud', 'tensile', 'embark', 'vagary', 'fanciful', 'feral', 'scour', 'exodus', 'expurgate', 'reassure', 'appurtenance', 'thereby', 'astral', 'gather', 'chastise', 'predecessor', 'lethal','good', 'bad']
 var playflag = false;
 var play_list = [[],[],[]];
 
@@ -100,24 +100,26 @@ var oxforddictionarycall = function(type,cb){
 
 var wordplay = function(){
     var hint_text = 'Here\'s a hint, ';
-    var game_test = '\nGuess the word: ';
+    var game_text = '\nGuess the word with the help of hint given! (use option \'hint\' for more hints)';
     var hint_flag = 0;
     var hint_count = [0,0,0];
     var user_input = 'hint';
+    console.log(game_text);
     while(true){
         if(user_input == 'hint'){
             if(hint_count[0]>=play_list[0].length && hint_count[1]>=play_list[1].length && hint_count[2]>=play_list[2].length){
                 console.log('It\'s sad to inform you that, you\'ve exhausted all the available hints!\nNext time, maybe!');
+                console.log('Word to be guessed was, "'+word.toUpperCase()+'"');
                 return;
             }
             console.log(hint_text + play_list[hint_flag][hint_count[hint_flag]%play_list[hint_flag].length%3]);
             hint_count[hint_flag]++;
             hint_flag = (hint_flag+1)%3;
         }
-        else if(user_input == 'try again'){
+        else if(user_input.trim().toLowerCase() == 'try again'){
             ;
         }
-        else if(user_input == 'quit'){
+        else if(user_input.trim().toLowerCase() == 'quit'){
             return;
         }
         else if(user_input.trim().toLowerCase() == word.toLowerCase()){
@@ -127,7 +129,13 @@ var wordplay = function(){
         else{
             console.log('Invalid Answer, try again!');
         }
-        user_input = readline.question("Your Input: ");
+        try{
+            user_input = readline.question("Your Input: ");
+        }
+        catch(exp){
+            console.log('Application\'s execution failed!');
+            process.exit(1);
+        }
     }
 }
 
@@ -224,7 +232,7 @@ var run_app = function(type){
         else if('wod' == type){
             oxforddictionarycall(word_arr[Math.floor(((Math.random() * 100) % word_arr.length))],0);
         }
-        else if('play' == type){
+        else if('play' == type && process.argv.length == 3){
             playflag=true;
             word = word_arr[Math.floor(((Math.random() * 100) % word_arr.length))];
             oxforddictionarycall(word,0);
